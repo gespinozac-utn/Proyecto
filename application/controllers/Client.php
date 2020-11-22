00e4ec2f81a4cdbab4448cd21ec92decc6182c20
gespinozac-utn <gespinozac@est.utn.ac.cr>
2020-11-21 21:22:24 -0600

    update directory
    add folder related to view clients

--- application/controllers/Client.php
+++ application/controllers/Client.php
@@ -5,14 +5,24 @@ class Client extends CI_Controller
 {
     function index()
     {
-        $this->load->view('templates/header');
-        $this->load->view('login');
-        $this->load->view('templates/footer');
+        $this->load->library('session');
+        if(!$this->session->has_userdata('user')){
+            $this->login();
+        }
     }
 
     function register(){
         $this->load->view('templates/header');
-        $this->load->view('register');
+        $this->load->view('client/register');
+        $this->load->view('templates/footer');
+    }
+
+    function login(){
+        $this->load->library('session');
+        $userRole = $this->session->has_userdata('user') ? $this->session->user : '';
+        
+        $this->load->view('templates/header',['userRole'=>$userRole]);
+        $this->load->view('client/login');
         $this->load->view('templates/footer');
     }
 }
